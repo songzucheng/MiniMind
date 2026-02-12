0e510101fa7d7df1fa8888ddf3599464ec4d2faf
songzucheng <1215768425@qq.com1215768425@qq.com>
2026-02-12 23:49:53 +0800

    rmsnorm

--- model/model.py
+++ model/model.py
@@ -69,3 +69,24 @@ class MokioMindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+
+import torch
+import torch.nn as nn
+
+# 继承nn.Module类
+class RMSNorm(nn.Module):
+   
+# __init__初始化
+    def __init__(self, dim:int, eps:float=1e-5):
+        super().__init__()
+        self.dim = dim 
+        self.eps = eps 
+        self.weight = nn.Parameter(torch.ones(dim))
+
+# _norm
+    def _norm(self, x):
+        return torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * x
+   
+# forward前向传播
+    def forward(self, x):
+        return self.weight*self._norm(x.float()).type_as(x)
